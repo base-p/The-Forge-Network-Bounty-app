c103ba96986b7b8898165df13b95f345971b31b8
base-p <okorieobumneke@gmail.com>
2018-02-05 00:45:09 +0100

    get posts

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -146,6 +146,21 @@ class UsersController extends AppController {
             'persistent_data_handler'=>'session'
           ]);
         
+        try {
+  // Returns a `Facebook\FacebookResponse` object
+              $response = $fb->get('me/feed?fields=created_time,link,message&limit=5', $accessToken);
+            } catch(Facebook\Exceptions\FacebookResponseException $e) {
+              echo 'Graph returned an error: ' . $e->getMessage();
+              exit;
+            } catch(Facebook\Exceptions\FacebookSDKException $e) {
+              echo 'Facebook SDK returned an error: ' . $e->getMessage();
+              exit;
+            }
+        
+        $feedEdge = $response->getGraphEdge();
+        foreach ($feedEdge as $graphNode) {
+  var_dump($graphNode);
+}
         
 	}
     

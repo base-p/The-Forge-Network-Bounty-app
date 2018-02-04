92c563422c185a804b2d8ca9258596ce10f806ee
base-p <okorieobumneke@gmail.com>
2018-02-04 04:14:19 +0100

    2

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -228,6 +228,20 @@ class UsersController extends AppController {
         }
 
         $_SESSION['fb_access_token'] = (string) $accessToken;
+        try {
+  // Returns a `Facebook\FacebookResponse` object
+  $response = $fb->get('/me?fields=id,name,email', $_SESSION['fb_access_token']);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$user = $response->getGraphUser();
+
+echo 'Name: ' . $user['name'];
 
         // User is logged in with a long-lived access token.
         // You can redirect them to a members-only page.
